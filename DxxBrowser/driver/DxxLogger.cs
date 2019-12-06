0e3e0ccd3590828dcff2c5c507516fd22a73df1d
toyota-m2k <mitsuki.toyota@metamoji.com>
2019-12-06 16:06:21 +0900

    ちょっと他の作業が割り込んだので。。。

---
+++ DxxBrowser/driver/DxxLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DxxBrowser.driver {
+    public class DxxLogInfo : DxxViewModelBase {
+        public enum LogType {
+            INFO,
+            WARN,
+            ERROR,
+        }
+
+        public DateTime Time { get; }
+        public LogType Type { get; }
+        public string Message { get; }
+
+        public DxxLogInfo(LogType type, string msg) {
+            Time = DateTime.Now;
+            Type = type;
+            Message = msg;
+        }
+    }
+
+    public class DxxLogger {
+        public 
+
+        private DxxLogger() {
+
+        }
+    }
+}
